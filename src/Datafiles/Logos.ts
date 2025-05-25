5f6ea9abc3a2242c512161e496bdcf689062565f
cmm25 <craigmutegi@gmail.com>
2025-05-26 01:20:42 +0300

    adjust

--- src/Datafiles/Logos.ts
+++
@@ -1,8 +0,0 @@
-export const logos = [
-    "Company 1",
-    "Company 2",
-    "Company 3",
-    "Company 4",
-    "Company 5",
-    "Company 6",
-];
--- src/components/auth/register/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./BasicInfoForm";
-export * from "./ConnectWallet";
-export * from "./EmailVerification";
-export * from "./IdentityVerification";
-export * from "./SkillCheckbox";
-export * from "./SkillsForm";
-export * from "./UserTypeSelection";
