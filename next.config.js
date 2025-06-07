7b126f90d2eca0eda541fc0758b744563b9f1b87
mercie-ux <mbaomercy0@gmail.com>
2025-06-07 07:40:41 +0300

    depends: Add Webpack config for SVG support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  /* Other config options can go here */
 };
 
-module.exports = nextConfig;
+module.exports = {
+  ...nextConfig,
+  webpack(config, { isServer }) {
+    // Modify the Webpack config for SVG handling
+    config.module.rules.push({
+      test: /\.svg$/, // Target SVG files
+      use: ['@svgr/webpack'], // Use @svgr/webpack to convert SVG to React components
+    });
+
+    // Return the modified config
+    return config;
+  },
+};
